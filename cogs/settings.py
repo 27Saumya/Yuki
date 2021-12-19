0b2dffaa8b7ed9bb8da2477d39d0a9e20597590d
Saumya Patel <patelsaumya767@gmail.com>
2021-12-19 09:55:18 +0400

    minor setting update

--- cogs/settings.py
+++ cogs/settings.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 from discord.commands import slash_command, Option
-import aiosqlite
 
 class SettingsCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
