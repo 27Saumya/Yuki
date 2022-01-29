28424fdbe49096624ce0d8c4868c31bd71448774
Saumya Patel <patelsaumya767@gmail.com>
2022-01-29 22:52:58 +0400

    Update buttons

--- utils/buttons.py
+++ utils/buttons.py
@@ -394,7 +394,8 @@ class TicketCloseTop2(discord.ui.View):
             child.disabled = True
         self.bot.dbcursor.execute('SELECT * FROM tickets WHERE guild_id=? AND channel_id=?', (interaction.guild_id, interaction.channel_id))
         member_id = self.bot.dbcursor.fetchone()
-        if member_id[2] not in await memberCheck(interaction.guild):
+        memCheck = await memberCheck(interaction.guild)
+        if member_id[2] not in memCheck:
             self.bot.dbcursor.execute(f'UPDATE tickets SET switch = "closed" WHERE guild_id=? AND channel_id=?', (interaction.guild_id, interaction.channel_id))
             self.bot.db.commit()
             await self.msg.delete()
@@ -445,7 +446,8 @@ class TicketControlsView(discord.ui.View):
             return await interaction.response.send_message(embed=discord.Embed(description=f"<:error:897382665781669908> You can't do that {interaction.user.mention}!", color=discord.Color.red()))
         self.bot.dbcursor.execute('SELECT * FROM tickets WHERE guild_id=? AND channel_id=?', (interaction.guild_id, interaction.channel_id))
         member_id = self.bot.dbcursor.fetchone()
-        if member_id[2] not in await memberCheck(interaction.guild):
+        memCheck = await memberCheck(interaction.guild)
+        if member_id[2] not in memCheck:
             return await interaction.response.send_message(embed=discord.Embed(description="**<:error:897382665781669908> This user is no more in this server!\n------------------------------------------\nThere is no use of opening this ticket!**", color=discord.Color.red()))
         member = interaction.guild.get_member(member_id[2])
         perms = interaction.channel.overwrites_for(member)
